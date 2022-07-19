558fc573c1053882d977ae8044ed36bdba810268
smugson <smugson499@gmail.com>
2022-07-19 09:52:32 +0100

    3 exercises done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0)
+        return "ERROR";
+    if (times === 0)
+        return "";
+    if (times === 1)
+        return string;
+    else
+        return string + repeatString(string, times -1);
 };
 
 // Do not edit below this line
